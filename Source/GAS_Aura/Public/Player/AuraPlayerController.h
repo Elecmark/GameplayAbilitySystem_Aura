a9ddfe3b5589f897f89e5bf5f6a2ed6e48a19fcf
Elecmark <944723112@qq.com>
2025-12-09 16:39:30 +0800

    Enhanced Input

---
+++ Source/GAS_Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// PU Dianheng
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
