52997d987c3e67c8ddd0150f025875adbfbd6792
Elecmark <944723112@qq.com>
2025-12-09 17:14:03 +0800

    Aura Player Controller

--- Source/GAS_Aura/Public/Player/AuraPlayerController.h
+++ Source/GAS_Aura/Public/Player/AuraPlayerController.h
@@ -4,7 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
 
 /**
  * 
@@ -13,5 +16,13 @@ UCLASS()
 class GAS_AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AAuraPlayerController();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
